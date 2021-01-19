b3f8b015d629430ff80142ffc06419cf9652c273
xeusteerapat <xeus085@gmail.com>
2021-01-20 00:07:00 +0700

    [EDIT] update agenda

--- cron/cron.js
+++ cron/cron.js
@@ -6,3 +6,18 @@ const agenda = new Agenda({
     address: process.env.MONGO_URI,
   },
 });
+
+agenda.define('log', job => {
+  console.log(job);
+  console.log('tst');
+});
+
+(async function () {
+  // IIFE to give access to async/await
+  await agenda.start();
+
+  // await agenda.every('3 minutes', 'log');
+
+  // Alternatively, you could also do:
+  await agenda.every('* * * * * *', 'log');
+})();
